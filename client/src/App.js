714bdc5129c09588cce97f8a556975906a2ebf3d
Pierrick Butin <pierrick.butin.v2@gmail.com>
2023-02-12 19:05:50 +0100

    Init project

---
+++ client/src/App.js
@@ -0,0 +1,87 @@
+import logo from './logo.svg';
+import './App.css';
+import socketIOClient from "socket.io-client";
+import React, { useState, useEffect } from "react";
+
+const socket = socketIOClient('localhost:3001');
+
+function App() {
+
+	const [tables, setTables] = useState([]);
+	const [points, setPoints] = useState([]);
+
+  useEffect(() => {
+    socket.on('initTables', tables => {
+      setTables(tables);
+    });
+
+    socket.on('removeAllTables', tableName => {
+      console.log('Received removeAllTables');
+      setTables([]);
+    });
+
+    socket.on('addTable', tableName => {
+      console.log('Received addTable');
+      setTables(tables => [...tables, tableName]);
+    });
+
+    socket.on('addPoints', pointsToAdd => {
+      console.log('Received addTable');
+      setPoints(points => [...points, pointsToAdd]);
+    });
+
+    return () => {
+      socket.off('initTables');
+      socket.off('removeAllTables');
+      socket.off('addTable');
+      socket.off('addPoints');
+    };
+  }, []);
+
+  const addTable = () => {
+    const nameInput = document.getElementById("tableNameInput");
+    if (nameInput.value !== "") {
+      console.log("Send addTable");
+      socket.emit("addTable", nameInput.value);
+      nameInput.value = "";
+    }
+  }
+
+  const removeAllTables = () => {
+    console.log("Send removeAllTables");
+    socket.emit("removeAllTables");
+  }
+
+  const addPoint = (points) => {
+    console.log("Send points");
+    socket.emit("addPoints", points);
+  }
+
+  return (
+    <div className="App">
+      <input type="text" id="tableNameInput" name="name" required></input>
+      <h3
+        onClick={() => addTable()}>
+          Ajouter une table
+      </h3>
+      <h5
+        onClick={() => removeAllTables()}>
+        Vider les tables
+      </h5>
+      <p>
+        Les tables :
+        { tables.map((table, index) =>
+          <li key={index}>
+            {table}
+          </li>
+        )}
+      </p>
+      {/* <h1
+        onClick={() => addPoint(10)}>
+          Ajouter des points
+      </h1> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.use(express.static(__dirname + '/client/build'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/client/build/index.html');
+});
+
+let tables=[];
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.emit('initTables', tables);
+
+    socket.on('addTable', (data) => {
+        console.log('addTable: ' + data);
+        tables.push(data);
+        io.emit('addTable', data);
+    });
+
+    socket.on('removeAllTables', () => {
+        console.log('removeAllTables: ');
+        tables = [];
+        io.emit('removeAllTables');
+    });
+
+    socket.on('addPoint', (data) => {
+        console.log('addPoint: ' + data);
+        io.emit('addPoint', data);
+    });
+
+    socket.on('disconnect', () => {
+      console.log('user disconnected');
+    });
+  });
+
+server.listen(3001, () => {
+  console.log('listening on *:3001');
+});
